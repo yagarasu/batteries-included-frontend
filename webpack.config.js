126e482e797de4b4c3de31ea4feebc6fbdac8cc3
Alexys Hegmann <alexys.hegmann@gmail.com>
2018-02-23 22:39:10 -0600

    Build scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,56 @@
 const path = require('path');
 const webpack = require('webpack');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: {
-    main: './src/index.js',
-    vendor: ['react', 'react-dom']
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['env', 'react'],
-          plugins: ['transform-object-rest-spread']
-        }
-      }
-    ]
-  },
-  resolve: {
-    modules: [
-      'node_modules',
-      path.resolve(__dirname, 'src')
-    ]
-  },
-  plugins: [
+module.exports = function (env) {
+  var plugins = [
+    new HtmlWebpackPlugin({
+      title: 'My App',
+      template: path.resolve(__dirname, 'src/index.html')
+    }),
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor'
-    }),
-    new BundleAnalyzerPlugin({
-      analyzerMode: 'static'
     })
   ]
+
+  if (env.analyzer) {
+    plugins.push(new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      reportFilename: path.resolve(__dirname, 'analysis/bundleReport.html'),
+      generateStatsFile: true,
+      statsFilename: path.resolve(__dirname, 'analysis/bundleStats.json')
+    }));
+  }
+
+  return {
+    entry: {
+      main: './src/index.js',
+      vendor: ['react', 'react-dom']
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          query: {
+            presets: ['env', 'react'],
+            plugins: ['transform-object-rest-spread']
+          }
+        }
+      ]
+    },
+    resolve: {
+      modules: [
+        'node_modules',
+        path.resolve(__dirname, 'src')
+      ]
+    },
+    plugins: plugins
+  };
 };
